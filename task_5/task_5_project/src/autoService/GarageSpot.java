69fe7afb05ea19a6b11cf4c64a3a1414e8959e1a
Pashos-2004 <pashos-2004@yandex.ru>
2025-11-23 17:05:57 +0300

    Task_5 project creation

---
+++ task_5/task_5_project/src/autoService/GarageSpot.java
@@ -0,0 +1,24 @@
+package autoService;
+
+class GarageSpot {
+	private int id;
+    private String box;
+    
+    public GarageSpot(int id , String box) {
+        this.id = id;
+    	this.box = box;
+    }
+    
+    public int getId() { 
+    	return id; 
+    }
+    
+    public String getBox() { 
+    	return box; 
+    }
+    
+    @Override
+    public String toString() {
+        return "GarageSpot{id=" + id + ", box='" + box + "}";
+    }
+}
